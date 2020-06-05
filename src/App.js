d1ec21da92b20325a3fab5a3c79032acabeb05d3
Stefan Berk <stefan@k2ff.de#>
2020-06-05 15:51:37 +0200

    BurgerBuilder basic setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Layout from "./components/Layouts/Layout"
+import React from "react";
+import Layout from "./components/Layouts/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder";
 
 function App() {
   return (
-    <div >
-    <Layout>
-      <p>Hello there, general Kenobi.</p>
+    <div>
+      <Layout>
+        <p>Hello there, general Kenobi.</p>
+        <BurgerBuilder/>
       </Layout>
     </div>
   );
