979f917389a5edddc6df9c4f926122ddaaa2ed50
Stefan Berk <stefan@k2ff.de#>
2020-06-06 09:56:17 +0200

    dynamic output of burger ingredients in the preview

--- src/components/Burger/BurgerPreview.js
+++ src/components/Burger/BurgerPreview.js
@@ -4,11 +4,17 @@ import Ingredient from './Ingredient/Ingredient'
 
 
 export default function burgerPreview(props) {
+    const ingredientsArr = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i) => {
+                return <Ingredient key={igKey + i} type={igKey} />;
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <Ingredient type="bread-top"/>
-            <Ingredient type="cheese"/>
-            <Ingredient type="meat"/>
+            {ingredientsArr}
             <Ingredient type="bread-bottom"/>
         </div>
     )
--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react'
-import Aux from '../hoc/Aux'
-import BurgerPreview from '../components/Burger/BurgerPreview'
+import React, { Component } from "react";
+import Aux from "../hoc/Aux";
+import BurgerPreview from "../components/Burger/BurgerPreview";
 
 export default class BurgerBuilder extends Component {
-    render() {
-        return (
-            <Aux>
-                <BurgerPreview />
-                <div>Build Controls</div>
-            </Aux>
-            
-        )
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 2,
+            meat: 4
+        }
     }
+  render() {
+    return (
+      <Aux>
+        <BurgerPreview ingredients={this.state.ingredients}/>
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
 }
