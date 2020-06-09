d0effa996debe07a5219133d0aea29950b8c0ded
Stefan Berk <stefan@k2ff.de#>
2020-06-09 16:16:57 +0200

    sideDrawer for mobile devices

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import classes from "./Toolbar.module.css"
-import Logo from '../../Logo/Logo'
-import NavigationItems from "./../NavigationItems/NavigationItems"
+import React from "react";
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "./../NavigationItems/NavigationItems";
 
 function toolbar() {
-    return (
-        <header className={classes.Toolbar}>
-            <div>MENU</div>
-           <Logo />
-           
-            <nav> <NavigationItems /> </nav>
-        </header>
-    )
+  return (
+    <header className={classes.Toolbar}>
+      <Logo height="80%" />
+
+      <nav className={classes.DesktopOnly}>
+        {" "}
+        <NavigationItems />{" "}
+      </nav>
+    </header>
+  );
 }
 
-export default toolbar
+export default toolbar;
