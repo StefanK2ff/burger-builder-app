c3714fb7f88e99220e04edbb842e3d522829826a
Stefan Berk <stefan@k2ff.de#>
2020-08-16 13:04:57 +0200

    basic setup of Order Overview (route + dummy order entry)

---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from "./Order.module.css"
+
+export default function Order(props) {
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: ...</p>
+            <p>Price ....</p>
+        </div>
+    )
+}
