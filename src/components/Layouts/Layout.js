509e286552d2384d053a3801df1c6015f663f7d6
Stefan Berk <stefan@k2ff.de#>
2020-10-12 07:41:56 +0200

    connectet Layout.js to redux state

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -1,27 +1,43 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-export default class Layout extends Component {
+class Layout extends Component {
   state = {
-    sideDrawerVisible: false
-  }
-  
+    sideDrawerVisible: false,
+  };
+
   sideDrawerHandler = () => {
     this.setState((prevState) => {
-      return {sideDrawerVisible: !prevState.sideDrawerVisible} 
-    })
-  }
+      return { sideDrawerVisible: !prevState.sideDrawerVisible };
+    });
+  };
 
   render() {
     return (
       <Aux>
-        <Toolbar toggleVisibilitySD={this.sideDrawerHandler}/>
-        <SideDrawer currentlyVisible={this.state.sideDrawerVisible} toggleVisibility={this.sideDrawerHandler}/>
+        <Toolbar
+          toggleVisibilitySD={this.sideDrawerHandler}
+          isAuthenticated={this.props.isAuthenticated}
+        />
+        <SideDrawer
+          currentlyVisible={this.state.sideDrawerVisible}
+          toggleVisibility={this.sideDrawerHandler}
+          isAuthenticated={this.props.isAuthenticated}
+        />
         <main className={classes.Content}>{this.props.children}</main>
       </Aux>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
