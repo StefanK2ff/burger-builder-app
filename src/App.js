3a837c62f6dbb739c1c6701893c839918f01cca9
Stefan Berk <stefan@k2ff.de#>
2020-10-17 16:59:09 +0200

    adding guarded routes, fixing log out bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Layout from "./components/Layouts/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import { Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
@@ -15,19 +15,40 @@ class App extends Component {
   }
 
   render() {
+    let routes = (
+      //default routes
+      <Switch>
+            <Route path="/auth" component={Auth} />
+            <Route path="/" component={BurgerBuilder} />
+            <Redirect to="/" />
+            </Switch>
+    );
+
+    if (this.props.isAuthed) {
+      routes = (
+        //routes if logged in
+        <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/" component={BurgerBuilder} />
+            <Redirect to="/" />
+            </Switch>
+      );
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthed: state.auth.token !== null,
   }
 }
 
@@ -37,4 +58,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -26,6 +26,9 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("expirationDate");
+  localStorage.removeItem("userId");
     return {
         type: actionTypes.AUTH_LOGOUT
     }
