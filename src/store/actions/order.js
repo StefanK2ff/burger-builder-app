98657035ab9b6aaba2d82ee014b76b8421c8d330
Stefan Berk <stefan@k2ff.de#>
2020-10-19 07:31:43 +0200

    jsut showing user's orders in 'my orders' by filtering in Backend request

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -60,11 +60,12 @@ export const ordersFetchStart = () => {
   };
 };
 
-export const ordersFetch = (token) => {
+export const ordersFetch = (token, userId) => {
   return (dispatch) => {
     dispatch(ordersFetchStart());
+    const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
     axios
-      .get("orders.json?auth=" + token)
+      .get('orders.json' + queryParams)
       .then((res) => {
         const fetchedOrders = [];
         for (let key in res.data) {
